3683b771f36b105a0aa3084d92a6eb079c1783a6
SOVEREIGN <isimemeni@gmail.com>
2020-10-25 14:28:07 -0400

    added player logic to game

---
+++ blackjack.py
@@ -0,0 +1,11 @@
+#BlackJack 
+
+#LOGIC -    
+# One deck of cards
+# Dealer is dealing 2 cards to player and 2 cards to themselves
+# If Dealer hits 21 on first go then Player loses
+# If Dealer hits 21 and player hits 21 on first go then no one wins
+# If player has less than 21 they can choose to hit for more cards or keep their hand
+# If Player hand is more than 21 then the Player loses
+# If Dealer hand hits higher then 21 then the Player wins
+# If the Dealers hand is closer to 21 than the player then the player loses
